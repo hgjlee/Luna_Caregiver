8a374973a489003c06ca8a0698fd1c506c538f0f
Jacob Lee <hyunjacoblee@gmail.com>
2017-11-16 18:05:55 -0500

    First Commit

---
+++ Simple Chat/Credentials.swift
@@ -0,0 +1,25 @@
+/**
+ * Copyright IBM Corporation 2017
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+ 
+public struct Credentials {
+    public static let ConversationUsername = "8e5605c7-5bcb-4d16-b5b6-34c832cb2a8b"
+    public static let ConversationPassword = "TKmjV3vwJ1TE"
+    public static let ConversationWorkspace = "f7c68e55-6473-41fe-add2-3b91c931d76b"
+    public static let SpeechToTextUsername = "7a19398d-5640-4165-bd0a-783b9cb85e1f"
+    public static let SpeechToTextPassword = "HL1bHCdB8gpt"
+    public static let TextToSpeechUsername = "acb58b4c-69f3-4b7d-a608-1179f7069862"
+    public static let TextToSpeechPassword = "Gzhsij5v1q3B"
+}
